2f1604164c8400b65c38bc7834761d1f0d5321d9
Tejas Khorana <khorana@khorana-macbookpro.roam.corp.google.com>
2020-04-14 20:51:17 -0700

    Attempt to utilize new Clarius APIs by using JNI. Attempt to import all necessary Qt dependencies as well.

--- hello-libs/app/src/main/java/com/example/hellolibs/MainActivity.java
+++ hello-libs/app/src/main/java/com/example/hellolibs/MainActivity.java
@@ -17,23 +17,27 @@ package com.example.hellolibs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 /*
  * Simple Java UI to trigger jni function. It is exactly same as Java code
  * in hello-jni.
  */
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.w("99899", "Starting!");
         TextView tv = new TextView(this);
-        tv.setText( stringFromJNI() );
+        Log.w("99899", "Found TextView, calling clariusIsConnected");
+        tv.setText("Port: " + clariusIsConnected());
+        Log.w("99899", "clariusIsConnected call successful, calling setContentView");
         setContentView(tv);
+        Log.w("99899", "finish");
     }
-    public native String  stringFromJNI();
-    static {
-        System.loadLibrary("hello-libs");
-    }
+    public native int clariusIsConnected();
 
+    static {
+        System.loadLibrary("listen");
+    }
 }
